56038ebe62a3533d604b5f3a0b5399d82022fd55
DoooongDong <rhkswls98@kakao.com>
2022-11-16 00:11:17 +0900

    엔티티 등록 - 트랜잭션을 지원하는 쓰기 지연

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -19,20 +19,15 @@ public class JpaMain {
          * WHY? 나머지 코드들은 스프링이 알아서 다 해준다.
          */
         try{
-            /**
-             * 비영속
-             */
-            Member member = new Member();
-            member.setId(101L);
-            member.setName("HelloJPA");
-
+            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(160L, "B");
             /**
              * 영속 - 이때 DB에 저장되는게 아니다. 영속 컨텍스트에 저장됨
              */
-            em.persist(member);
-            Member findMember = em.find(Member.class, 101L);
-            System.out.println("findMember.getId() = " + findMember.getId());
-            System.out.println("findMember.getName() = " + findMember.getName());
+            em.persist(member1);
+            em.persist(member2);
+
+            System.out.println("================");
 
             /**
              * DB는 영속 한 이후 commit 을 해야 저장됨
