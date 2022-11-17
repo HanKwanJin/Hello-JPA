eac874dbd8ede247772d84d43d7f67051c76871e
DoooongDong <rhkswls98@kakao.com>
2022-11-17 23:01:34 +0900

    객체지향스럽지 않은 매핑

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -3,29 +3,29 @@ package hellojpa;
 import javax.persistence.*;
 import java.util.Date;
 
-@Entity
+//@Entity
 public class Member {
     //PK
-    @Id
-    private Long id;
-
-    @Column(name = "name")
-    private String username;
-
-    private Integer age;
-
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-
-    @Lob
-    private String description;
-
-    public Member(){}
+//    @Id
+//    private Long id;
+//
+//    @Column(name = "name")
+//    private String username;
+//
+//    private Integer age;
+//
+//    @Enumerated(EnumType.STRING)
+//    private RoleType roleType;
+//
+//    @Temporal(TemporalType.TIMESTAMP)
+//    private Date lastModifiedDate;
+//
+//    @Temporal(TemporalType.TIMESTAMP)
+//    private Date createdDate;
+//
+//    @Lob
+//    private String description;
+//
+//    public Member(){}
 
 }
